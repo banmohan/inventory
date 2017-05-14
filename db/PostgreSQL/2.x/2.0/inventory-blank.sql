cf9131aeffd1239bbc6b97cb3d2a79d19658bcde
Mohan Ban <banmohan@outlook.com>
2017-05-15 01:48:44 +0545

    Serial number updates

--- db/PostgreSQL/2.x/2.0/inventory-blank.sql
+++ db/PostgreSQL/2.x/2.0/inventory-blank.sql
@@ -3749,12 +3749,15 @@ SELECT
     checkouts.transaction_master_id,
     serial_numbers.batch_number,
     serial_numbers.serial_number,
-    serial_numbers.expiry_date
+    serial_numbers.expiry_date,
+	CASE WHEN COALESCE(transaction_master.verification_status_id, 0) >= 0 THEN
+		serial_numbers.sales_transaction_id END AS sales_transaction_id
 FROM inventory.serial_numbers
 JOIN inventory.items ON serial_numbers.item_id = items.item_id
 JOIN inventory.units ON serial_numbers.unit_id = units.unit_id
 JOIN inventory.stores ON serial_numbers.store_id = stores.store_id
 JOIN inventory.checkouts ON serial_numbers.checkout_id = checkouts.checkout_id
+LEFT JOIN finance.transaction_master ON serial_numbers.sales_transaction_id = transaction_master.transaction_master_id
 WHERE NOT serial_numbers.deleted;
 
 
