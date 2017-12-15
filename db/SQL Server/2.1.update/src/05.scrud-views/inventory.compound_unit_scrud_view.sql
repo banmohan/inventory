3c18850f44cd3c41cb737f5cd1d75b932f581b06
binodnp@outlook.com <binodnp@outlook.com>
2017-12-15 21:57:59 +0545

    Routine development.

---
+++ db/SQL Server/2.1.update/src/05.scrud-views/inventory.compound_unit_scrud_view.sql
@@ -0,0 +1,20 @@
+﻿IF OBJECT_ID('inventory.compound_unit_scrud_view') IS NOT NULL
+DROP VIEW inventory.compound_unit_scrud_view;
+
+GO
+
+CREATE VIEW inventory.compound_unit_scrud_view
+AS
+SELECT
+    compound_unit_id,
+    base_unit.unit_name base_unit_name,
+    value,
+    compare_unit.unit_name compare_unit_name
+FROM
+    inventory.compound_units,
+    inventory.units base_unit,
+    inventory.units compare_unit
+WHERE inventory.compound_units.base_unit_id = base_unit.unit_id
+AND inventory.compound_units.compare_unit_id = compare_unit.unit_id;
+
+GO
---
+++ db/SQL Server/2.1.update/src/05.scrud-views/inventory.item_scrud_view.sql
@@ -0,0 +1,45 @@
+﻿IF OBJECT_ID('inventory.item_scrud_view') IS NOT NULL
+DROP VIEW inventory.item_scrud_view;
+
+GO
+
+
+
+CREATE VIEW inventory.item_scrud_view
+AS
+SELECT
+    inventory.items.item_id,
+    inventory.items.item_code,
+    inventory.items.item_name,
+    inventory.items.barcode,
+    inventory.items.item_group_id,
+    inventory.item_groups.item_group_name,
+    inventory.item_types.item_type_id,
+    inventory.item_types.item_type_name,
+    inventory.items.brand_id,
+    inventory.brands.brand_name,
+    inventory.suppliers.supplier_name,
+    inventory.items.unit_id,
+    inventory.units.unit_code,
+    inventory.units.unit_name,
+    inventory.items.hot_item,
+    inventory.items.cost_price,
+    inventory.items.selling_price,
+    inventory.items.maintain_inventory,
+    inventory.items.allow_sales,
+    inventory.items.allow_purchase,
+    inventory.items.photo
+FROM inventory.items
+LEFT JOIN inventory.item_groups
+ON inventory.item_groups.item_group_id = inventory.items.item_group_id
+LEFT JOIN inventory.item_types
+ON inventory.item_types.item_type_id = inventory.items.item_type_id
+LEFT JOIN inventory.brands
+ON inventory.brands.brand_id = inventory.items.brand_id
+LEFT JOIN inventory.units
+ON inventory.units.unit_id = inventory.items.unit_id
+LEFT JOIN inventory.suppliers
+ON inventory.suppliers.supplier_id = inventory.items.preferred_supplier_id
+WHERE inventory.items.deleted = 0;
+
+GO
